f7cc78bacd46eee913cf642447d64457ed2c105c
Vijay Rathod <rathod.vjy@gmail.com>
2016-10-19 13:59:24 +0200

    Adding Initial structure with Grunt

---
+++ TrackRe-web/TrackReTest/app/modules/about/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackReTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trackReTestApp
+ */
+angular.module('trackReTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ TrackRe-web/TrackReTest/app/modules/main/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackReTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trackReTestApp
+ */
+angular.module('trackReTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
